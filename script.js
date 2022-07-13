0311999db0657d2b6ed8da69affe051834ea36aa
FRANCO1357 <fmelani13@gmail.com>
2022-07-13 17:30:42 +0200

    milestone 3

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ Vue.config.devtools = true;
 
 const root = new Vue({
     name: 'ToDoList',
-    el: '#list',
+    el: '#list-container',
     data: {
+        newTask: '',
         tasks: [
             {text: 'Tagliarsi i capelli', done: true,},
             {text: 'Tagliarsi la barba', done: false,},
@@ -14,6 +15,10 @@ const root = new Vue({
     methods: {
         deleteTask(task){
             this.tasks.splice(task, 1);
+        },
+        addTask(){
+            this.tasks.push({text: this.newTask, done: false});
+            this.newTask = '';
         },
         doneTask(i){
             this.tasks[i].done = true;
